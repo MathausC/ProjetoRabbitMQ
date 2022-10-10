78695fd5d5b4152072f32c479f890e697b94937d
MathausC <mathaus273@gmail.com>
2022-10-10 01:16:06 -0300

    Update RabbitMQConstants.java

--- src/main/java/br/unicap/programacao_paralela/connections/RabbitMQConstants.java
+++ src/main/java/br/unicap/programacao_paralela/connections/RabbitMQConstants.java
@@ -1,5 +1,6 @@
 package br.unicap.programacao_paralela.connections;
 
 public final class RabbitMQConstants {
-    public static final String NOMA_FILA = "Chat";
+    public static final String USUARIO = "Almir";
+    public static final String DESTINO = "Mathaus";
 }
