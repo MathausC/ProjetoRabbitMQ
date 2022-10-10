160c25f0c4fbfe3f9c06b9c7b5347211fa054e3b
MathausC <mathaus273@gmail.com>
2022-10-09 22:55:00 -0300

    Create RabbitMQConnection class

---
+++ src/main/java/br/unicap/programacao_paralela/connections/RabbitMQConnection.java
@@ -0,0 +1,42 @@
+package br.unicap.programacao_paralela.connections;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class RabbitMQConnection {
+
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private static final String NOME_EXCHANGE = "amq_direct";
+
+    private Queue fila(String nomeFile) {
+        return new Queue(nomeFile, true, false, false);
+    }
+
+    private DirectExchange trocaDireta() {
+        return new DirectExchange(NOME_EXCHANGE, true, false);
+    }
+
+    private Binding relacionamento(Queue fila, DirectExchange trocaDireta) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, trocaDireta.getName(), fila.getName(), null);
+    }
+
+    @PostConstruct
+    private void adiciona(){
+        Queue fila = this.fila(RabbitMQConstants.NOMA_FILA);
+        DirectExchange troca = this.trocaDireta();
+
+        this.amqpAdmin.declareQueue(fila);
+        this.amqpAdmin.declareExchange(troca);
+    }
+}
